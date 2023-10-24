d0961ac75955eb65f4a293eb54a060712719ab99
LiZi510524 <142325508+LiZi510524@users.noreply.github.com>
2023-10-24 19:54:54 +0800

    Add files via upload

---
+++ 循环练习(阶乘相关的）.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;     
+//	int n = 0;
+//	int ret = 1;
+//	scanf("%d", &n);     //“%d''不能加\n(原因暂不清楚）
+//	for(i=1;i<=n;i++)   //点乘不能有0，故因赋值为1
+//	{
+//		ret = ret * i;
+//	}
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//#include<stdio.h >            算出来是n的阶乘并不是之和（未解决为什么）
+//int main()
+//{
+//	int a = 0;
+//	int n = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	scanf("%d", &n);
+//	for(a=1;a>=n;a++)
+//	{
+//		ret = 1;
+//		for (a = 1; a >= n; a++)
+//		{
+//			ret = ret * a;
+//		}
+//		sum=sum+ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//#include<stdio.h>         求n的阶乘之和
+//int main()
+//{
+//	int a = 0;
+//	int n = 0;
+//	int sum = 0;
+//	int ret = 1;
+//	scanf("%d", &n);
+//	for (a = 1; a <= n; a++)
+//	{
+//		ret = ret * a;
+//		sum = sum + ret;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+
