23070516efe96f7161613a9babb9051332892c7a
LiZi510524 <142325508+LiZi510524@users.noreply.github.com>
2023-11-04 17:12:42 +0800

    Add files via upload

---
+++ 函数/add.h
@@ -0,0 +1,20 @@
+#pragma once
+int add(int x, int y)    //自己的函数库   addition
+{ 
+	return x + y;
+}
+
+int sub(int x, int y)    //subtraction
+{
+	return x - y;
+}
+
+int mul(int x, int y)   //multiplicatin
+{
+	return x * y;
+}
+
+int div(int x, int y)   //divsion
+{
+	return x / y;
+}
