ebdb6df70fe86e6e5d7814bb2b64e09e77198a77
LiZi510524 <142325508+LiZi510524@users.noreply.github.com>
2023-11-04 17:16:02 +0800

    Add files via upload

---
+++ 函数/函数的嵌套.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 43;
+//	printf("%d", printf("%d", printf("%d", 43)));    //最初打印的是43，后面嵌套打印的是元素个数
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+	/*int b = strlen("abcdef");
+	printf("%d", b);*/
+
+	//链式访问
+	printf("%d\n", strlen("abcdef"));
+	return 0;
+}
