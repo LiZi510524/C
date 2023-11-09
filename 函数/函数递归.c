62d537f6ad202d28b32fa8d463604aed41be8396
LiZi510524 <142325508+LiZi510524@users.noreply.github.com>
2023-11-09 21:09:39 +0800

    Add files via upload

---
+++ 函数/函数递归.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//void print(unsigned int n)   //此处是print而不是printf如果使用printf则会发生错误（真的想哭，看了好多遍都没发现这个问提
+//{
+//	if (n > 9)
+//	{
+//		print(n/10);
+//	}
+//	printf("%d ", n%10);
+//}
+//
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%u", &num);
+//	print(num);
+//	return 0;
+//
+//}
+
