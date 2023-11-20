d73ed4527c1982344839c18fda0322a09a211c56
LiZi510524 <142325508+LiZi510524@users.noreply.github.com>
2023-11-20 21:05:02 +0800

    Add files via upload

---
+++ 数组/一维数组.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+	/*char ch1[] = "abc";*/
+//	char ch2[3] = {'a','b','c'};
+//	int arr3[] = { 1,2,3 };
+//	char ch4[10] = "abc";
+//	char ch5[10] = { 'a','b','c' };
+//	int arr6[10] = { 1,2,3 };
+//	/*printf("%s\n", ch1);*/
+//	printf("%s\n", ch2);
+//	printf("%d\n", arr3);
+//	printf("%s\n", ch4);
+//	printf("%s\n", ch5);
+//	printf("%d\n", arr6);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5,6,7,8,9 };
+//	int n = sizeof(arr1) / sizeof(arr1[0]);
+//	printf("%d  ", n);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = { 'a','b','c' };
+//	char arr2[] = "abc";  //字符串类型结尾有“\0''
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	char arr[10] = { 'a','b','c'};
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("&arr[%d]=%p\n", i, &arr[i]);  //每4个为间隔  字符型为1
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	float b = 1.1;
+//	char c = "d";
+//	printf("%p\n%p\n%p\n", b,c,a);
+//	return 0;
+//}
